4cb75fc4ab5de2b36802e12304949bc453c0cdc6
JIAJINSIM <JJSIMYOU@GMAIL.COM>
2024-07-23 01:55:00 +0800

    Update deploy.sh to correct the volume path

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -5,8 +5,8 @@ set -x
 docker stop my-apache-php-app || true
 docker rm my-apache-php-app || true
 
-# Run the container with the volume path formatted for Windows
-docker run -d -p 80:80 --name my-apache-php-app -v /c/Users/dclit/OneDrive/Documents/GitHub/SSDLAB7B/jenkins/scripts/src:/var/www/html php:7.2-apache
+# Run the container with the correct volume path
+docker run -d -p 80:80 --name my-apache-php-app -v /c/Users/dclit/OneDrive/Documents/GitHub/SSDLAB7B/jenkins/scripts/src/src:/var/www/html php:7.2-apache
 
 # Give it a moment to start up
 sleep 1
