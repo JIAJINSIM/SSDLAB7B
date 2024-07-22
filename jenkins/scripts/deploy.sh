84043a4d176b7cdf0ed78301f43fd31089e95751
JIAJINSIM <JJSIMYOU@GMAIL.COM>
2024-07-23 01:00:06 +0800

    test

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,19 +1,18 @@
 #!/usr/bin/env sh
 
 set -x
-docker stop my-apache-php-app || true
-docker rm my-apache-php-app || true
+docker-compose down
 
-# Use simplified path for testing
-docker run -d -p 80:80 --name my-apache-php-app -v /c/Users/dclit/temp-html:/var/www/html php:7.2-apache
+# Start the service using docker-compose
+docker-compose up -d
 
 # Sleep to allow the container to start
 sleep 5
 
 # List the files in the container to verify
-docker exec my-apache-php-app ls -la /var/www/html
+docker-compose exec web ls -la /var/www/html
 
 set +x
 
 echo 'Now...'
-echo 'Visit http://localhost to see your PHP application in action.'
+echo 'Visit http://localhost:8080 to see your PHP application in action.'
