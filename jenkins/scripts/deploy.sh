5a713c7cf515ed389560b4cb6bd8e4132636d5fc
JIAJINSIM <JJSIMYOU@GMAIL.COM>
2024-07-23 01:15:04 +0800

    test revert

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,18 +1,9 @@
 #!/usr/bin/env sh
 
 set -x
-docker-compose down
-
-# Start the service using docker-compose
-docker-compose up -d
-
-# Sleep to allow the container to start
-sleep 5
-
-# List the files in the container to verify
-docker-compose exec web ls -la /var/www/html
-
+docker run -d -p 80:80 --name my-apache-php-app -v /c/Users/dclit/OneDrive/Documents/GitHub/SSDLAB7B/jenkins/scripts/src:/var/www/html php:7.2-apache
+sleep 1
 set +x
 
 echo 'Now...'
-echo 'Visit http://localhost:8081 to see your PHP application in action.'
+echo 'Visit http://localhost to see your PHP application in action.'
