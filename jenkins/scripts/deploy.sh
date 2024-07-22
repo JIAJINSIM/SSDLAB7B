58fc1faff00883b716d034df97ffbdaddfa8f12e
JIAJINSIM <JJSIMYOU@GMAIL.COM>
2024-07-23 01:09:16 +0800

    test

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -4,13 +4,13 @@ set -x
 docker-compose down
 
 # Start the service using docker-compose
-MSYS_NO_PATHCONV=1 docker-compose up -d
+docker-compose up -d
 
 # Sleep to allow the container to start
 sleep 5
 
 # List the files in the container to verify
-MSYS_NO_PATHCONV=1 docker-compose exec web ls -la /var/www/html
+docker-compose exec web ls -la /var/www/html
 
 set +x
 
