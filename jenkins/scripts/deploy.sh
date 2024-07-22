6b9058a2f00ae8e71e85a4d61e302fdef1ba1f35
JIAJINSIM <JJSIMYOU@GMAIL.COM>
2024-07-23 01:38:55 +0800

    test

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -4,10 +4,17 @@ set -x
 # Stop and remove any existing containers
 docker stop my-apache-php-app || true
 docker rm my-apache-php-app || true
-docker run -d -p 80:80 --name my-apache-php-app -v C:\\Users\\dclit\\OneDrive\\Documents\\GitHub\\SSDLAB7B\\jenkins\\scripts\\src:/var/www/html php:7.2-apache
+
+# Run the container with the volume path formatted for Windows
+docker run -d -p 80:80 --name my-apache-php-app -v /c/Users/dclit/OneDrive/Documents/GitHub/SSDLAB7B/jenkins/scripts/src:/var/www/html php:7.2-apache
+
+# Give it a moment to start up
 sleep 1
 set +x
 
+# Print a message to visit the application
 echo 'Now...'
 echo 'Visit http://localhost to see your PHP application in action.'
 
+# Verify if the container is running
+docker ps
