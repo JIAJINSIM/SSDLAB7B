f14f1a4ba440aed0d26a7996dbb3866d8ca75750
JIAJINSIM <JJSIMYOU@GMAIL.COM>
2024-07-23 00:43:07 +0800

    test

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -4,8 +4,8 @@ set -x
 docker stop my-apache-php-app || true
 docker rm my-apache-php-app || true
 
-# Corrected path syntax for Windows
-docker run -d -p 80:80 --name my-apache-php-app -v /c/Users/dclit/OneDrive/Documents/GitHub/SSDLAB7B/jenkins/scripts/src:/var/www/html php:7.2-apache
+# Using a simpler path for testing
+docker run -d -p 80:80 --name my-apache-php-app -v /c/Users/dclit/temp-html:/var/www/html php:7.2-apache
 
 sleep 1
 set +x
