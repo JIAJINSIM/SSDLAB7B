c9d2e5c152ea8a601ecb33f25f50f6c12151407a
JIAJINSIM <JJSIMYOU@GMAIL.COM>
2024-07-23 00:45:13 +0800

    test

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -4,8 +4,11 @@ set -x
 docker stop my-apache-php-app || true
 docker rm my-apache-php-app || true
 
-# Using a simpler path for testing
-docker run -d -p 80:80 --name my-apache-php-app -v /c/Users/dclit/temp-html:/var/www/html php:7.2-apache
+# Corrected path syntax for Windows
+docker run -d -p 80:80 --name my-apache-php-app -v /c/Users/dclit/OneDrive/Documents/GitHub/SSDLAB7B/jenkins/scripts/src:/var/www/html php:7.2-apache
+
+# Adjust permissions within the Docker container
+docker exec my-apache-php-app chmod -R 755 /var/www/html
 
 sleep 1
 set +x
