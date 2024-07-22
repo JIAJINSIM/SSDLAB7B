4c830c7905f188c521a7aad00f8cee5352aa3dee
JIAJINSIM <JJSIMYOU@GMAIL.COM>
2024-07-23 00:52:33 +0800

    aa

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -4,7 +4,7 @@ set -x
 docker stop my-apache-php-app || true
 docker rm my-apache-php-app || true
 
-# Using a simpler path for testing
+# Corrected path syntax for Windows
 docker run -d -p 80:80 --name my-apache-php-app -v /c/Users/dclit/temp-html:/var/www/html php:7.2-apache
 
 # Sleep to allow the container to start
@@ -12,6 +12,9 @@ sleep 5
 
 # Adjust permissions within the Docker container
 docker exec my-apache-php-app sh -c "chmod -R 755 /var/www/html"
+
+# List the files in the container to verify
+docker exec my-apache-php-app ls -la /var/www/html
 
 set +x
 
