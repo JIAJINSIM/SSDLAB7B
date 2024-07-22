6a64c4e2d99b8c77326782931dc3dd9aaa0777c0
JIAJINSIM <JJSIMYOU@GMAIL.COM>
2024-07-23 00:25:58 +0800

    test

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env sh
 
 set -x
+docker stop my-apache-php-app || true
+docker rm my-apache-php-app || true
 docker run -d -p 80:80 --name my-apache-php-app -v /c/Users/dclit/OneDrive/Documents/GitHub/SSDLAB7B/jenkins/scripts/src:/var/www/html php:7.2-apache
 sleep 1
 set +x
